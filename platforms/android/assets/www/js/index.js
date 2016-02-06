6983abcbba89539deb3304d0cbea12b380d56d4e
oscar.shrimpton.personal@gmail.com <oscar.shrimpton.personal@gmail.com>
2016-02-06 11:27:32 +0000

    Stuff

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -12,6 +12,32 @@ var app = {
     		alert('This app is only available online.');
     		navigator.app.exitApp();
     	}
-        // TODO: Notification Code
+
+        var Fetcher = window.plugins.backgroundFetch;
+
+        var fetchCallback = function() {
+
+
+        }
+
+        Fetcher.configure(fetchCallback);        
+    },
+
+    getNotifications: function(){
+
+        $.get('http://forum.bexcellent.org.uk/notifications.json', function(data){
+            if (data.errors !== undefined){
+                navigator.notification.alert('Error getting notifications!');
+            }else{
+                for (notification in data.notifications){
+                    if (!notification.read){
+                        // TODO: Display notification properly
+                        navigator.notification.alert('You have a notification!');
+                    }
+                }            
+            }
+        });
     }
+
+
 };
--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,43 @@
 var app = {
-    initialize: function() {
-        this.bindEvents();
-    },
+	initialize: function() {
+		this.bindEvents();
+	},
 
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
 
-    onDeviceReady: function() {
-    	if (navigator.connection.type == Connection.NONE || navigator.connection.type == Connection.UNKNOWN){
-    		alert('This app is only available online.');
-    		navigator.app.exitApp();
-    	}
-        // TODO: Notification Code
-    }
+	onDeviceReady: function() {
+		if (navigator.connection.type == Connection.NONE || navigator.connection.type == Connection.UNKNOWN){
+			alert('This app is only available online.');
+			navigator.app.exitApp();
+		}
+
+		var Fetcher = window.BackgroundFetch;
+
+		var fetchCallback = function() {
+
+
+			$.get('http://forum.bexcellent.org.uk/notifications.json', function(data){
+				if (data.errors !== undefined){
+					navigator.notification.alert('Error getting notifications!');
+				}else{
+					for (notification in data.notifications){
+						if (!notification.read){
+							navigator.notification.alert('You have a notification!');
+						}
+					}
+				}
+				Fetcher.finish();
+
+			});
+		}
+		var failureCallback = function(){
+			console.log('Failed Background Fetch - What does this mean?'); // TODO
+		}
+		Fetcher.configure(fetchCallback, failureCallback);
+	}
+}
+
+
 };
